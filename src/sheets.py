4481f5d52fb1c979363263dd20c0004321faec2d
Joe Marshall <joemarshall97@live.co.uk>
2023-12-22 12:33:41 +0000

    Implemented function to clean raw transaction export

--- src/sheets.py
+++ src/sheets.py
@@ -1,20 +1,23 @@
-import gspread
-
-from gspread import Worksheet
 from datetime import datetime
+
+import gspread
 from google.oauth2 import service_account
+from gspread import Worksheet
+
 
 class SheetOperations:
     """Class used to interact with Google Sheets API."""
 
-    def __init__(self, sheetname: str, worksheet: int|str) -> None:
+    def __init__(self, sheetname: str, worksheet: int | str) -> None:
         self.scope = [
             "https://spreadsheets.google.com/feeds",
             "https://www.googleapis.com/auth/spreadsheets",
             "https://www.googleapis.com/auth/drive.file",
-            "https://www.googleapis.com/auth/drive"
-            ]
-        self.creds = service_account.Credentials.from_service_account_file(filename="google_creds.json", scopes=self.scope)
+            "https://www.googleapis.com/auth/drive",
+        ]
+        self.creds = service_account.Credentials.from_service_account_file(
+            filename="google_creds.json", scopes=self.scope
+        )
         self.client = gspread.authorize(self.creds)
         self.sheetname = sheetname
         self.worksheet = worksheet
@@ -23,19 +26,21 @@ class SheetOperations:
         """Method to open designated worksheet."""
 
         return self.client.open(self.sheetname).get_worksheet(self.worksheet)
-    
+
     def get_row_count(self) -> int:
         """Method to get total number of rows in sheet."""
 
         return self.open_sheet().row_count
-    
+
     def get_row_data(self, row_num: int) -> None:
         """Method returns data from specified row number."""
 
         return self.open_sheet().row_values(row_num)
-    
+
     def get_last_entry_date(self) -> str:
         """Method returns last entry date in sheet."""
-        
+
         col_vals = [x for x in self.open_sheet().col_values(2) if x != ""]
-        return datetime.strptime(col_vals[-1], "%d/%m/%Y").strftime("%Y-%m-%dT%H:%M:%SZ")
+        return datetime.strptime(col_vals[-1], "%d/%m/%Y").strftime(
+            "%Y-%m-%dT%H:%M:%SZ"
+        )
