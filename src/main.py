4b0205634708f6e69922a7d2fb4a3c601639263a
Joe Marshall <joemarshall97@live.co.uk>
2024-04-13 12:39:43 +0100

    Removed StarlingAPI class and replaced with function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 from sheets import SheetOperations
-from starling import AccountOperations, StarlingAPI, clean_export
-
-URL = "https://api.starlingbank.com/api/v2/"
+from starling import AccountOperations, clean_export, gen_starling_api_headers
 
 
 def main() -> None:
     """Main function to call Starling API and return account balance."""
-    headers = StarlingAPI().default_headers
+    headers = gen_starling_api_headers()
     ws = SheetOperations(sheetname="Budget Automation Test", worksheet=4)
-    account = AccountOperations(URL, headers)
+    account = AccountOperations(headers)
 
     last_date = ws.get_last_entry_date()
     transactions = account.export_transactions(date=last_date)
